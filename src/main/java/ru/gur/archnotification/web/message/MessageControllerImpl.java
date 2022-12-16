59c8ff31c67ec2c05f7b9084e59c6725bd819068
Yuriy Gorelov <gur-ok@yandex.ru>
2022-12-16 17:17:00 +0400

    some fixes

--- src/main/java/ru/gur/archnotification/web/message/MessageControllerImpl.java
+++ src/main/java/ru/gur/archnotification/web/message/MessageControllerImpl.java
@@ -1,9 +1,11 @@
 package ru.gur.archnotification.web.message;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gur.archnotification.service.MessageService;
@@ -24,18 +26,21 @@ public class MessageControllerImpl implements MessageController {
 
     @Override
     @GetMapping(path = "/{id}")
-    public GetMessageResponse read(final UUID id) {
+    @Operation(summary = "Получить сообщение по идентификатору")
+    public GetMessageResponse read(@PathVariable final UUID id) {
         return conversionService.convert(messageService.read(id), GetMessageResponse.class);
     }
 
     @Override
-    @GetMapping(path = "/account/{id}")
-    public GetMessageResponse findByAccountId(final UUID accountId) {
+    @GetMapping(path = "/account/{accountId}")
+    @Operation(summary = "Получить сообщения по аккаунту")
+    public GetMessageResponse findByAccountId(@PathVariable final UUID accountId) {
         return conversionService.convert(messageService.read(accountId), GetMessageResponse.class);
     }
 
     @Override
     @GetMapping
+    @Operation(summary = "Получить все сообщения")
     public List<GetMessageResponse> getMessages() {
         return messageService.getMessages().stream()
                 .map(m -> conversionService.convert(m, GetMessageResponse.class))
@@ -44,6 +49,7 @@ public class MessageControllerImpl implements MessageController {
 
     @Override
     @DeleteMapping
+    @Operation(summary = "Удалить все сообщения")
     public void clear() {
         messageService.deleteAll();
     }
